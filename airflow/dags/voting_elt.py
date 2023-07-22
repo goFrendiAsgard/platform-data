bce2e1d995b7b6ffe40856efad44cfd24ccb68bc
goFrendi <gofrendiasgard@gmail.com>
2023-07-22 16:28:50 +0700

    add airflow dag

--- airflow/dags/voting_elt.py
+++ airflow/dags/voting_elt.py
@@ -7,9 +7,15 @@ import pendulum
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
+import os
+
+
+AIRBYTE_CONN_ID = 'airbyte_voting'
+CONNECTION_ID = os.getenv('VOTING_AIRBYTE_CONNECTION_ID', 'aad992b4-e014-4854-955e-2e65ddf3f051')
 
 with DAG(
-    dag_id="example_bash_operator",
+    dag_id="voting_elt",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
@@ -17,42 +23,22 @@ with DAG(
     tags=["example", "example2"],
     params={"example_key": "example_value"},
 ) as dag:
-    run_this_last = EmptyOperator(
-        task_id="run_this_last",
+    # https://docs.airbyte.com/operator-guides/using-the-airflow-airbyte-operator/
+    sync_voting = AirbyteTriggerSyncOperator(
+        task_id='sync_voting',
+        airbyte_conn_id=AIRBYTE_CONN_ID,
+        connection_id=CONNECTION_ID,
+        asynchronous=False,
+        timeout=3600,
+        wait_seconds=3
     )
 
-    # [START howto_operator_bash]
-    run_this = BashOperator(
-        task_id="run_after_loop",
+    transform = BashOperator(
+        task_id="transform",
         bash_command="echo 1",
     )
-    # [END howto_operator_bash]
 
-    run_this >> run_this_last
-
-    for i in range(3):
-        task = BashOperator(
-            task_id="runme_" + str(i),
-            bash_command='echo "{{ task_instance_key_str }}" && sleep 1',
-        )
-        task >> run_this
-
-    # [START howto_operator_bash_template]
-    also_run_this = BashOperator(
-        task_id="also_run_this",
-        bash_command='echo "ti_key={{ task_instance_key_str }}"',
-    )
-    # [END howto_operator_bash_template]
-    also_run_this >> run_this_last
-
-# [START howto_operator_bash_skip]
-this_will_skip = BashOperator(
-    task_id="this_will_skip",
-    bash_command='echo "hello world"; exit 99;',
-    dag=dag,
-)
-# [END howto_operator_bash_skip]
-this_will_skip >> run_this_last
+    sync_voting >> transform
 
 if __name__ == "__main__":
     dag.test()
