4d339ea2ab6f930ab0bcb0ba96c7cc7267d2426c
goFrendi <gofrendiasgard@gmail.com>
2023-07-22 16:52:15 +0700

    add airflow dag

--- airflow/dags/voting_elt.py
+++ airflow/dags/voting_elt.py
@@ -6,7 +6,6 @@ import pendulum
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.empty import EmptyOperator
 from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
 import os
 
@@ -15,13 +14,13 @@ AIRBYTE_CONN_ID = 'airbyte_voting'
 CONNECTION_ID = os.getenv('VOTING_AIRBYTE_CONNECTION_ID', 'aad992b4-e014-4854-955e-2e65ddf3f051')
 
 with DAG(
-    dag_id="voting_elt",
-    schedule="0 0 * * *",
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    dag_id='voting_elt',
+    schedule='0 0 * * *',
+    start_date=pendulum.datetime(2021, 1, 1, tz='UTC'),
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=60),
-    tags=["example", "example2"],
-    params={"example_key": "example_value"},
+    tags=['example', 'example2'],
+    params={'example_key': 'example_value'},
 ) as dag:
     # https://docs.airbyte.com/operator-guides/using-the-airflow-airbyte-operator/
     sync_voting = AirbyteTriggerSyncOperator(
@@ -34,11 +33,17 @@ with DAG(
     )
 
     transform = BashOperator(
-        task_id="transform",
-        bash_command="echo 1",
+        task_id='transform',
+        bash_command=' && '.join([
+            'export CLICKHOUSE_HOST=host.docker.internal',
+            'cd /opt/airflow/dags',
+            'source prepare.sh', 
+            'cd dbt_project', 
+            'dbt run && dbt test',
+        ]) 
     )
 
     sync_voting >> transform
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     dag.test()
